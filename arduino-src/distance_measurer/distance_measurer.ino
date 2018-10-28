ff1717ef7cbd5ac8cdd3b807582dd0ead679084b
John Taylor <jaydeetay@users.noreply.github.com>
2018-10-28 14:09:11 -0400

    Initial version. Unsmoothed.

--- arduino-src/distance_measurer/distance_measurer.ino
+++ arduino-src/distance_measurer/distance_measurer.ino
@@ -1,21 +1,28 @@
 /*
- * Code to pull the distance from an ultrasonic sensor
- * and show it on a 4 digit display.
- */
+   Code to pull the distance from an ultrasonic sensor
+   and show it on a 4 digit display.
+*/
 
- #include <TM1637Display.h>
+#include <SR04.h>
+#include <TM1637Display.h>
 
- const int DISP_CLK = 9;
- const int DISP_DIO = 8;
+const int DISP_CLK = 9;
+const int DISP_DIO = 8;
+const int SENS_TRIG = 10;
+const int SENS_ECHO = 11;
 
- TM1637Display display(DISP_CLK, DISP_DIO);
+TM1637Display display(DISP_CLK, DISP_DIO);
+SR04 sr04 = SR04(SENS_ECHO, SENS_TRIG);
 
 void setup() {
+  Serial.begin(9600);
   display.setBrightness(0x0a); // Max brightness
-
 }
 
 void loop() {
-  display.showNumberDec(999);
-  delay(1000);
+  long dist_cm = sr04.Distance();
+  Serial.print(dist_cm);
+  Serial.println("cm");
+  display.showNumberDec(dist_cm);
+  delay(100);
 }
