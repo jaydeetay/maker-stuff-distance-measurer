3c89b1b8422dd481eb5733e7402ed928ff1f7881
John Taylor <jaydeetay@users.noreply.github.com>
2018-10-28 14:21:50 -0400

    Added smoothing.

--- arduino-src/distance_measurer/distance_measurer.ino
+++ arduino-src/distance_measurer/distance_measurer.ino
@@ -10,6 +10,7 @@ const int DISP_CLK = 9;
 const int DISP_DIO = 8;
 const int SENS_TRIG = 10;
 const int SENS_ECHO = 11;
+const int SLEEP_TIME_MILLIS = 10;
 
 TM1637Display display(DISP_CLK, DISP_DIO);
 SR04 sr04 = SR04(SENS_ECHO, SENS_TRIG);
@@ -19,10 +20,30 @@ void setup() {
   display.setBrightness(0x0a); // Max brightness
 }
 
+class Smoother {
+  public:
+    Smoother(double lambda) : last_value_(-1), lambda_(lambda) {}
+    long smooth_me(long value) {
+      if (last_value_ == -1) {
+        last_value_ = value;
+        return value;
+      } else {
+        long new_value = lambda_ * value + (1-lambda_) * last_value_;
+        last_value_ = new_value;
+        return new_value;
+      }
+    }
+    private:
+    long last_value_;
+    double lambda_;
+};
+
+Smoother smoother(0.5);
+
 void loop() {
-  long dist_cm = sr04.Distance();
+  long dist_cm = smoother.smooth_me(sr04.Distance());
   Serial.print(dist_cm);
   Serial.println("cm");
   display.showNumberDec(dist_cm);
-  delay(100);
+  delay(SLEEP_TIME_MILLIS);
 }
